3f6e357557b379af97242e568e3aa779f19fbe66
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-18 21:48:46 -0400

    add backgroundImage

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Space Grotesk, sans-serif',
+      },
+      backgroundImage: {
+        'main-desktop': 'url(/bg-main-desktop.png)',
+        'main-mobile': 'url(/bg-main-mobile.png)',
+        'card-back': 'url(/bg-card-back.png)',
+        'card-front': 'url(/bg-card-front.png)',
+      },
+    },
   },
   plugins: [],
 };
